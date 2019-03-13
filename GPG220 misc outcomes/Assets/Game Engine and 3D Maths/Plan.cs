c0f8f4fba1322a4d516b9cb176c4d129eee991cb
Michael <1013532@student.sae.edu.au>
2019-03-13 16:40:31 +1100

    plans and rider plugin

---
+++ GPG220 misc outcomes/Assets/Game Engine and 3D Maths/Plan.cs
@@ -0,0 +1,10 @@
+﻿/*
+ * Intermediate development skillset (LOs 10­11)
+ * 
+ * Particle systems:
+ * 
+ * Intermediate animations ­ Bones, skinning, morphs, keyframe interpolation, forward­ and inverse­kinematics:
+ * 
+ * 2D and 3D audio:
+ * 
+ */
---
+++ GPG220 misc outcomes/Assets/intermediate AI/Planned.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Intermediate AI (LOs 7­9)
+ *
+ * Group AI:
+ * flocking behaviour
+ *
+ * Strategy and Tactical Ai:
+ *
+ * Agents:
+ *
+ * Goal oriented planning system:
+ * link to strategy and tactical AI
+ *
+ * Pathfinding:
+ * build on Vestra model script
+ *
+ * Overview of broader AI and its use for problem-solving:
+ *
+ * 
+ */
