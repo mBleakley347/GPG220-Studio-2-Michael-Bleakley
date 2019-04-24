7d0b593270134b8ae88cab1809b6e1f66367cec5
Michael <1013532@student.sae.edu.au>
2019-04-24 13:57:36 +1000

    CIU Group Project

---
+++ GPG220 misc outcomes/Assets/CIU Project Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerJump;
+    public float playerRotation;
+    public float playerSpeed;
+
+	void Update () {
+
+        //Jump
+        if (Input.GetKeyDown("space"))
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, playerJump, 0), ForceMode.Impulse);
+        }
+
+        // rotate player based on mouse movement
+        float RotationHorizontal = Input.GetAxis("Mouse X") * playerRotation;
+        transform.Rotate(0, RotationHorizontal, 0);
+
+        // move player based on key inputs
+        var Straifing = Input.GetAxis("Horizontal") * ((playerSpeed * transform.localScale.y)/4);
+        float Vertical = Input.GetAxis("Vertical") * ((playerSpeed * transform.localScale.y)/2);
+        transform.Translate(Straifing, 0, Vertical);
+    }
+}
